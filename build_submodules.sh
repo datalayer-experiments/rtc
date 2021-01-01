8cb69e849ca35a7be5319a74d16869dad700b1da
Eric Charles <eric@datalayer.io>
2021-01-01 12:18:02 +0100

    gitmodules

--- build_submodules.sh
+++ build_submodules.sh
@@ -1,60 +1,75 @@
 #!/bin/sh
 
-# From the root of rtc-experiments folder.
+# From the root of the repository folder.
 root_dir=`pwd`
 
+# Checkout the correct submodules branches
+function checkout() {
 
-## Checkout the correct submodules branches
+  cd $root_dir/externals/automerge && \
+    git fetch origin && \
+    git checkout datalayer
 
-cd externals/automerge && \
-  git fetch origin && \
-  git checkout datalayer && \
-  cd $root_dir
+  cd $root_dir/externals/automerge-performance && \
+    git fetch origin && \
+    git checkout datalayer-performance
 
-cd externals/automerge-performance && \
-  git fetch origin && \
-  git checkout datalayer-performance && \
-  cd $root_dir
+  cd $root_dir/externals/automerge-wasm-nodejs && \
+    git fetch origin && \
+    git checkout wasm-nodejs
 
-cd externals/automerge-wasm && \
-  git fetch origin && \
-  git checkout wasm-node-local-change && \
-  cd $root_dir
+  cd $root_dir/externals/automerge-wasm-bundler && \
+    git fetch origin && \
+    git checkout wasm-bundler
 
-cd externals/automerge-rs-nodejs && \
-  git fetch origin && \
-  git checkout datalayer-nodejs && \
-  cd $root_dir
+  cd $root_dir/externals/automerge-rs-nodejs && \
+    git fetch origin && \
+    git checkout datalayer-nodejs
 
-cd externals/automerge-rs-bundler && \
-  git fetch origin && \
-  git checkout datalayer-bundler && \
-  cd $root_dir
+  cd $root_dir/externals/automerge-rs-bundler && \
+    git fetch origin && \
+    git checkout datalayer-bundler
 
-## Build submodules
+}
 
-cd externals/automerge && \
-  yarn && \ # Install node modules.
-  yarn build && \ # Build node modules.
-  cd $root_dir
+function build_automerge() {
 
-cd externals/automerge-performance && \
-  yarn && \ # Install node modules.
-  yarn build && \ # Build node modules.
-  cd $root_dir
+  cd $root_dir/externals/automerge && \
+    yarn && \ # Install node modules.
+    yarn build # Build node modules.
 
-cargo install wasm-pack
+  cd $root_dir/externals/automerge-performance && \
+    yarn && \ # Install node modules.
+    yarn build # Build node modules.
 
-cd externals/automerge-rs-nodejs && \
-  cargo build && \
-  cd automerge-backend-wasm && \
-  yarn install && \
-  yarn build && \
-  cd $root_dir
+#  cd $root_dir/externals/automerge-wasm-node && \
+#    yarn && \ # Install node modules.
+#    yarn build # Build node modules.
 
-cd externals/automerge-rs-bundler && \
-  cargo build && \
-  cd automerge-backend-wasm && \
-  yarn install && \
-  yarn build && \
-  cd $root_dir
+#  cd $root_dir/externals/automerge-wasm-bundler && \
+#    yarn && \ # Install node modules.
+#    yarn build # Build node modules.
+
+}
+
+function build_automerge_rs() {
+
+  cargo install wasm-pack
+
+  cd $root_dir/externals/automerge-rs-nodejs && \
+    cargo build && \
+    cd automerge-backend-wasm && \
+    yarn install && \
+    yarn build
+
+  cd $root_dir/externals/automerge-rs-bundler && \
+    cargo build && \
+    cd automerge-backend-wasm && \
+    yarn install && \
+    yarn build
+
+}
+
+checkout
+build_automerge
+# build_automerge_rs
