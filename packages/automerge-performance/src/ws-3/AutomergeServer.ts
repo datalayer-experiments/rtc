9ef87c038bf7bb29d7459157fa679499e9128594
Eric Charles <eric@datalayer.io>
2021-01-29 16:31:51 +0100

    wip

--- packages/automerge-performance/src/ws-3/AutomergeServer.ts
+++ packages/automerge-performance/src/ws-3/AutomergeServer.ts
@@ -1,6 +1,7 @@
 import WebSocket from 'ws'
 
 import Automerge, { Text } from 'automerge'
+import { IncomingMessage } from 'http'
 
 const http = require('http')
 
@@ -9,8 +10,13 @@ const wsReadyStateOpen = 1
 
 export const docs = new Map<string, WSSharedDoc>()
 
+type CursorPerUser = {
+  [userName: string]: Automerge.Cursor
+}
+
 type AMString = {
-  value: Text
+  text: Text
+  cursors: CursorPerUser
 }
 
 const broadcastChanges = (conn, doc: WSSharedDoc, changes: Uint8Array[]) => {
@@ -18,7 +24,7 @@ const broadcastChanges = (conn, doc: WSSharedDoc, changes: Uint8Array[]) => {
     onClose(doc, conn, null)
   }
   try {
-    console.log('--- Broadcast Changes', changes)
+    console.log('--- Broadcast Changes:', changes)
     changes.map(change => conn.send(change, err => { err != null && onClose(doc, conn, err) }))    
   } catch (e) {
     onClose(doc, conn, e)
@@ -35,10 +41,10 @@ class WSSharedDoc {
 }
 
 const onMessage = (currentConn, docName, sharedDoc: WSSharedDoc, message: any) => {
-  const change = new Uint8Array(message)
-  console.log('--- Change', change)
+  const change = new Uint8Array(message) 
+  console.log('--- Change:', change)
   sharedDoc.doc = Automerge.applyChanges(sharedDoc.doc, [change])
-  console.log('--- Doc Value', sharedDoc.doc.value.toString())
+  console.log('--- Current Doc Value:', sharedDoc.doc.text.toString())
   sharedDoc.conns.forEach((_, conn) => {
     if (currentConn != conn ) {
       broadcastChanges(conn, sharedDoc, [change])
@@ -46,17 +52,25 @@ const onMessage = (currentConn, docName, sharedDoc: WSSharedDoc, message: any) =
   })
 }
 
-export const getSharedDoc = (docName: string): WSSharedDoc => {
-  const k = docs.get(docName)
+export const getSharedDoc = (userId: string, docName: string): WSSharedDoc => {
+  let k = docs.get(docName)
   if (k) {
+    k.doc = Automerge.change(k.doc, s => {
+      s.cursors[userId] = s.text.getCursorAt(1)
+    })
     return k
   }
-  let doc = Automerge.init<AMString>()
+  let doc = Automerge.init<AMString>({ actorId: userId})
+//  let doc = Automerge.from<AMString>({ value: new Text() })
   doc = Automerge.change(doc, s => {
-    s.value = new Text()
-    s.value.insertAt(0, ...'hello string')
-    s.value.deleteAt(0, 1)
-    s.value.insertAt(0, 'H')
+    s.text = new Text()
+    s.text.insertAt(0, ...'hello string')
+    s.text.deleteAt(0, 1)
+    s.text.insertAt(0, 'H')
+    s.cursors = {}
+  })
+  doc = Automerge.change(doc, s => {
+    s.cursors[userId] = s.text.getCursorAt(1)
   })
   const sharedDoc = new WSSharedDoc(doc)
   docs.set(docName, sharedDoc)
@@ -71,12 +85,13 @@ const onClose = (conn, doc: WSSharedDoc, err) => {
   conn.close()
 }
 
-const setupWSConnection = (conn, req, { 
-  docName = req.url.slice(1).split('?')[0] as string
-} = {}) => {
-  console.log('Setup WS Connection', docName)
+const setupWSConnection = (conn, req: IncomingMessage) => {
+  const urlPath = req.url.slice(1).split('?')[0]
+  const userId = urlPath.split('/')[0]
+  const docName = urlPath.split('/')[1]
+  console.log('Setup WS Connection', userId, docName)
   conn.binaryType = 'arraybuffer'
-  const sharedDoc = getSharedDoc(docName)
+  const sharedDoc = getSharedDoc(userId, docName)
   const changes = Automerge.getChanges(Automerge.init<AMString>(), sharedDoc.doc)
   broadcastChanges(conn, sharedDoc, changes);
   sharedDoc.conns.set(conn, new Set())
